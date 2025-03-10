d02e71e8ca8ceea21ac3ca3b16e777d90649493a
gowthaman <gowthaman.kutty1197@gmail.com>
2025-03-10 22:51:18 +0530

    Improved app performance

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestraunt] = useState([]);
@@ -40,6 +41,16 @@ const Body = () => {
 
     setFilteredRestaurant(filterSearch);
   };
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you are offline. Please check your internet connection.
+      </h1>
+    );
+
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,22 @@
 import React from "react";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const User = () => {
   const [users] = useRestaurantMenu(
     "https://api.github.com/users/gowthaman1197"
   );
+  const onlineStatus = useOnlineStatus();
+
   if (users === null) {
     return <p>Loading...</p>;
   }
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you are offline. Please check your internet connection.
+      </h1>
+    );
   return (
     <>
       <img src={users.avatar_url} />
